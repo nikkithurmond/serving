5d79089e78e0eef46d1b171c8970f434ba1ef07a
Joseph Burnett <josephburnett79@gmail.com>
2018-06-20 20:11:39 +0000

    Replace concurrencyModel with instanceMaxRequestConcurrency.

--- pkg/apis/serving/v1alpha1/revision_types.go
+++ pkg/apis/serving/v1alpha1/revision_types.go
@@ -91,7 +91,7 @@ const (
 	RevisionRequestConcurrencyModelMulti RevisionRequestConcurrencyModelType = "Multi"
 )
 
-type RevisionInstanceConcurrencyType int64
+type RevisionInstanceRequestMaxConcurrencyType int64
 
 // RevisionSpec holds the desired state of the Revision (from the client).
 type RevisionSpec struct {
@@ -116,13 +116,13 @@ type RevisionSpec struct {
 	// +optional
 	ConcurrencyModel RevisionRequestConcurrencyModelType `json:"concurrencyModel,omitempty"`
 
-	// InstanceConcurrency specifies the maximum allowed
+	// InstanceMaxRequestConcurrency specifies the maximum allowed
 	// in-flight (concurrent) requests per instance of the Revision
 	// container. Defaults to `0` which means the system should
 	// decide. This field replaces ConcurrencyModel. A value of `1`
 	// is equivalent to `Single` and `0` is equivalent to `Multi`.
 	// +optional
-	InstanceConcurrency RevisionInstanceConcurrencyType `json:"instanceConcurrency,omitempty"`
+	InstanceMaxRequestConcurrency RevisionInstanceRequestMaxConcurrencyType `json:"instanceMaxConcurrency,omitempty"`
 
 	// ServiceAccountName holds the name of the Kubernetes service account
 	// as which the underlying K8s resources should be run. If unspecified
